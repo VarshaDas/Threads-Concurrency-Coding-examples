ed5dfed7db1d788a2d1d47853f85df4925dbfb45
Varsha <varsha.das@cerner.com>
2023-09-08 17:32:09 +0530

    concurrent hashmap

--- Threads/src/main/java/Threads/ConcurrentHashmapDemo.java
+++ Threads/src/main/java/Threads/ConcurrentHashmapDemo.java
@@ -2,41 +2,36 @@ package Threads;
 
 import java.util.HashMap;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class ConcurrentHashmapDemo {
     private static final int NUM_THREADS = 5;
     private static final int NUM_INSERTIONS = 100;
-    private static HashMap<String, Integer> hashMap = new HashMap<>();
-    public static void main(String[] args) {
-        Runnable insertTask = () -> {
-            for (int i = 0; i < NUM_INSERTIONS; i++) {
-                int key = i;
-                int value = i;
+    private static ConcurrentHashMap<String, Integer> hashMap = new ConcurrentHashMap<>();
 
-                // Insert the key-value pair into the HashMap without synchronization
-                hashMap.put(key + Thread.currentThread().getName(), value);
+    public static void main(String[] args) throws InterruptedException {
+
+        ExecutorService executorService = Executors.newFixedThreadPool(NUM_THREADS);
+
+        for(int i = 0; i< NUM_THREADS;i++) {
+            executorService.execute(insertRecord());
+        }
+
+        executorService.shutdown();
+        if(!executorService.isTerminated()){
+            Thread.sleep(1000);
+        }
+
+        System.out.println("Size of the hashmap = " +hashMap.size());
+
+    }
+
+    private static Runnable insertRecord() {
+        return () -> {
+            for(int i = 0 ; i<NUM_INSERTIONS; i++){
+                hashMap.put(i + Thread.currentThread().getName() , i);
             }
         };
-
-        // Create and start multiple threads to insert data concurrently
-        Thread[] threads = new Thread[NUM_THREADS];
-        for (int i = 0; i < NUM_THREADS; i++) {
-            threads[i] = new Thread(insertTask);
-            threads[i].start();
-        }
-
-        // Wait for all threads to finish
-        for (Thread thread : threads) {
-            try {
-                thread.join();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-
-        // Print the size of the HashMap after concurrent insertions
-        System.out.println("Size of the HashMap: " + hashMap.size());
     }
 }
-
-
