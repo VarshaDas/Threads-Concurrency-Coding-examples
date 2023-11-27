242b8ea4487438f697cad5ceb203b13218d1380c
megha <meghasharma.msharma@gmail.com>
2023-11-27 20:12:35 +0530

    CachedThreadPool demo

--- Threads/src/main/java/Threads/CachedThreadPoolDemo.java
+++ Threads/src/main/java/Threads/CachedThreadPoolDemo.java
@@ -1,4 +1,16 @@
 import java.util.concurrent.*;
+
+/*
+ *
+ * This program uses a cached thread pool to process a large number of tasks. 
+ * We test its performance and observe how the thread pool adjusts the number of threads based on the workload.
+ * 
+ * Here, we will create a Cached Thread Pool, and submit 15 tasks to the pool. 
+ * Each task prints its ID and the name of the executing thread.
+ * 
+ * Since a cached thread pool can dynamically adjust the number of threads based on the workload, it is observed that initially, a few threads will be created to process tasks. 
+ * As tasks complete, threads may be reused for new tasks, and if the workload is consistently high, the pool may create new threads up to a maximum limit.
+ */
 public class CachedThreadPoolDemo {
     public static void main(String[] args) {
         //Create a cached thread pool
